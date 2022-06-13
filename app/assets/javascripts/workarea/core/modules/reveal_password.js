92331f1fc6d8ef96a2ae5257ad560f60f833b4fe
deepak raghuwanshi <deepak.r@trikatechnologies.com>
2022-06-13 13:45:10 +0530

    Override a Javascript module from Storefront -Override the reveal_password module from workarea storefront, override the JST template referenced within the module (JST templates are overridden in the same way as JS modules) -Update the markup in the JST to be a single button -Update the module to store the current state in the data attribute on the button when it is clicked -Use the stored state on the button to determine whether the password should be shown or hidden -Use I18n.t() to change the text content of the button depending on the current state

---
+++ app/assets/javascripts/workarea/core/modules/reveal_password.js
@@ -0,0 +1,46 @@
+/**
+ * @namespace WORKAREA.revealPassword
+ */
+ WORKAREA.registerModule('revealPassword', (function () {
+  'use strict';
+
+  var buttonTemplate = JST['workarea/core/templates/reveal_password_button'],
+
+      toggleButton = function ($field, event) {
+          var $button = $(event.currentTarget);
+
+          if ($button.data('revealPassword') === 'show') {
+              $button.data('revealPassword', 'hide')
+              $field.attr('type', 'password');
+          } else {
+              $button.data('revealPassword', 'show')
+              $field.attr('type', 'text');
+          }
+      },
+
+      bindButtonEvents = function ($field) {
+          var $container = $field.closest('.property');
+
+          $('[data-reveal-password]', $container)
+          .on('click', _.partial(toggleButton, $field));
+      },
+
+      injectButtons = function (index, field) {
+          return $(field).after(buttonTemplate());
+      },
+
+      initRevealPassword = _.flow(injectButtons, bindButtonEvents),
+
+      /**
+       * @method
+       * @name init
+       * @memberof WORKAREA.revealPassword
+       */
+      init = function ($scope) {
+          $('input[type=password]', $scope).each(initRevealPassword);
+      };
+
+  return {
+      init: init
+  };
+}()));
